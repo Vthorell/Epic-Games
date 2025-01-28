e3a0805fb0e5acfd93e574db1931dc4bafbe5b79
Viggo Thorell <viggotho@gmail.com>
2025-01-28 09:29:00 +0100

    fullgrid-startpage commit

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,26 @@ const db = new Database('./db/epic-games.db', { fileMustExist: true });
 
 router.get('/', (req, res) => {
   // Hämta totalt minst 10 bilder från databasen
-  const allImages = db.prepare('SELECT productImage FROM products LIMIT 10').all();
+  const allImages = db.prepare('SELECT productImage FROM products LIMIT 26').all();
 
   // Dela upp bilderna i grupper om 3, 5 och 2
-  const group1 = allImages.slice(0, 3); // 3 bilder
-  const group2 = allImages.slice(3, 8); // 5 bilder
-  const group3 = allImages.slice(8, 10); // 2 bilder
+  const group1 = allImages.slice(0, 3); 
+  const group2 = allImages.slice(3, 8); 
+  const group3 = allImages.slice(8, 11); 
+  const group4 = allImages.slice(11, 16);
+  const group5 = allImages.slice(16, 18);
+  const group6 = allImages.slice(18, 23);
+  const group7 = allImages.slice(23, 26);
 
-  // Skicka grupperna till EJS
   res.render('index', {
     title: 'Epic Games Store',
     group1,
     group2,
     group3,
+    group4,
+    group5,
+    group6,
+    group7,
   });
 });
 
